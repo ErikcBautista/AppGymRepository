4386970319083d2f62e5513865c8f6b678dc2cc5
ErikcBautista <65507591+ErikcBautista@users.noreply.github.com>
2022-05-20 17:49:48 -0500

    Bug bitacora corregido

--- AppGym/AppGym/Models/GymDatabaseContext.cs
+++ AppGym/AppGym/Models/GymDatabaseContext.cs
@@ -27,7 +27,7 @@ namespace AppGym.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=DESKTOP-GKG9MVL;Initial Catalog=GymDatabase;Integrated Security=True");
             }
         }
